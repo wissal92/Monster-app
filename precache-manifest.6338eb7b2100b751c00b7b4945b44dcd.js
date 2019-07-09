26452029e27e216395ad9e40c1caa8a5d1db2e5b
wissal <wissal.el@gmail.com>
2019-07-09 15:14:32 -0700

    Updates

---
+++ precache-manifest.6338eb7b2100b751c00b7b4945b44dcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd510fb9d048ab4c8dcb4cf0096e95c",
+    "url": "/Monster-app/index.html"
+  },
+  {
+    "revision": "0e885c445ec8540f3e63",
+    "url": "/Monster-app/static/css/main.b8a52dc4.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/Monster-app/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "0e885c445ec8540f3e63",
+    "url": "/Monster-app/static/js/main.75640735.chunk.js"
+  },
+  {
+    "revision": "957fd6ce92bb0bc885d5",
+    "url": "/Monster-app/static/js/runtime~main.319a2a61.js"
+  }
+]);
